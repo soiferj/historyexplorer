aa6bc81b9c7b922c619b85bbfbecb1de6d47a476
soiferj <soiferj@gmail.com>
2025-05-04 22:51:44 -0700

    fix space

--- client/src/App.js
+++ client/src/App.js
@@ -46,7 +46,7 @@ function App() {
                 </div>
             </div>
             {/* Main content centered below header */}
-            <div className="flex flex-col items-center w-full max-w-5xl px-8 mx-auto mt-8 mb-4 z-10 min-h-0">
+            <div className="flex flex-col items-center w-full max-w-5xl px-8 mx-auto mt-2 mb-4 z-10 min-h-0">
                 <Timeline user={session?.user} accessToken={session?.access_token} />
             </div>
         </div>
